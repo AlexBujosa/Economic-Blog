dd52fdc68cc9111e3bf422fc7b35ea740e1b9cb3
SahiraTejada <Sahira2208@hotmail.com>
2022-12-28 17:06:16 -0400

    Done Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
+import { Login } from './pages/Login';
+
 
 const App = () => {
   return (
-    <div className='container'>
+    <div >
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={Login} />
         <Route path='/blog/:id' component={Blog} />
         <Redirect to='/' />
       </Switch>
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './styles.css';
+export const Login = () => {
+  return (
+    <div>
+        <div className='login'>
+            <div className='title'>
+              <h1 className='blog-name'>Runo</h1>
+              <div className='line'/>
+            </div>
+            <div className='login-form'>
+                <form action="">
+                    <p>Bienvenido de nuevo</p>
+                    <h2 style={{marginBottom:'30px'}}>Iniciar sesión en su cuenta</h2>
+                    <div>
+                    <div className='container-input' style={{marginBottom:'20px'}}>
+                      <p className='label-input'>Correo</p>
+                      <input className='form-input' type="email"/>
+                    </div>
+                     <div>
+                        <p className='label-input'>Contraseña</p>
+                        <input className='form-input' type="password"/>
+                    </div>
+                    </div>
+                     <button className='btn-login' type="submit">Login</button>
+                </form>
+                <div className='sign-in'>
+                  <p >¿No tienes una cuenta?<span>Registrate</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
