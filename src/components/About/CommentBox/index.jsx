71f9bef99c7f0d17e9b970583399c59e966c50e9
ismaeldcent04 <87149086+ismaeldcent04@users.noreply.github.com>
2022-12-29 21:04:18 -0400

    again

---
+++ src/components/About/CommentBox/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import "./styles.css";
+
+const CommentBox = () => (
+  <div>
+    <form>
+      <input />
+      <Button>Add</Button>
+    </form>
+  </div>
+);
+
+export default CommentBox;
---
+++ src/components/About/CommentSection/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CommentBox from "../CommentBox";
+import "./styles.css";
+
+const CommentSection = () => (
+  <div className="comments">
+    <CommentBox />
+  </div>
+);
+
+export default CommentSection;
