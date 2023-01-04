fc5e73f7cc08faeb0eb1e5164d875b3c69139322
OSCAR JOSE DURAN CUELLO <odurancuello@gmail.com>
2023-01-04 04:16:51 -0400

    Comments API integration

--- src/components/Blog/CommentBox/index.jsx
+++ src/components/Blog/CommentBox/index.jsx
@@ -1,38 +1,58 @@
-import React, { useState } from "react";
-import { Button, Avatar } from "@material-ui/core";
-import "./styles.css";
+import React, { useState } from 'react';
+import { Button, Avatar } from '@material-ui/core';
+import './styles.css';
 
-const CommentBox = (props) => {
-  const [comment, setcomment] = useState("");
+const CommentBox = () => {
+	const [formData, setFormData] = useState({});
 
-  function handleChange(event) {
-    const value = event.target.value;
-    setcomment(value);
-  }
+	const postComment = async (e) => {
+		e.preventDefault();
+		await fetch('https://economic-blog-backend.up.railway.app/api/comments', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(formData),
+		})
+			.then((res) => res.json())
+			.then(() => {
+				resetFormData();
+				window.location.reload();
+			});
+	};
 
-  function handlesubmit(event) {
-    event.preventDefault();
-    props.onAdd(comment);
-    setcomment("");
-  }
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+			user: localStorage.getItem('username'),
+			article: window.location.href.substring(
+				window.location.href.lastIndexOf('/') + 1
+			),
+		});
+	};
 
-  return (
-    <div>
-      <form onSubmit={handlesubmit}>
-        <div className="commentBox">
-          <Avatar />
-          <input
-            value={comment}
-            onChange={handleChange}
-            placeholder="Add a comment..."
-          />
-        </div>
-        <Button type="submit" onClick={handlesubmit}>
-          Comment
-        </Button>
-      </form>
-    </div>
-  );
+	const resetFormData = () => {
+		setFormData({});
+	};
+
+	return (
+		<div>
+			<form onSubmit={postComment}>
+				<div className="commentBox">
+					<Avatar />
+					<input
+						value={formData.body || ''}
+						onChange={handleChange}
+						placeholder="Add a comment..."
+						name="body"
+					/>
+				</div>
+				<Button type="submit">Comment</Button>
+			</form>
+		</div>
+	);
 };
 
 export default CommentBox;
--- src/components/Blog/CommentSection/index.jsx
+++ src/components/Blog/CommentSection/index.jsx
@@ -1,47 +1,34 @@
-import React, { useState } from "react";
-import CommentBox from "../CommentBox";
-import CommentPost from "../CommentPost";
-import "./styles.css";
-
-// const comments = [
-//   {
-//     id: 1,
-//     name: "Juan Perez",
-//     body: "Me gusto mucho el articulo",
-//   },
-//   {
-//     id: 2,
-//     name: "Ismael Dicent",
-//     body: "Me gusto mucho el articulo",
-//   },
-//   {
-//     id: 3,
-//     name: "Alex Bujosa",
-//     body: "Eh mentiraaa",
-//   },
-//   {
-//     id: 4,
-//     name: "Sahira Tejada",
-//     body: "Uhmm esta interesante",
-//   },
-// ];
+import React, { useState, useEffect } from 'react';
+import CommentBox from '../CommentBox';
+import CommentPost from '../CommentPost';
+import './styles.css';
 
 const CommentSection = () => {
-  const [comments, setComments] = useState([]);
+	const [comments, setComments] = useState([]);
 
-  function addComment(com) {
-    setComments((prevValue) => {
-      return [...prevValue, com];
-    });
-  }
-  return (
-    <div className="comments">
-      <CommentBox onAdd={addComment} />
-      {comments.map((comment, index) => {
-        return <CommentPost key={index} name="Ismael" body={comment} />;
-      })}
-    </div>
-  );
+	const getComments = async () => {
+		await fetch(
+			'https://economic-blog-backend.up.railway.app/api/comments/article/' +
+				window.location.href.substring(
+					window.location.href.lastIndexOf('/') + 1
+				)
+		)
+			.then((response) => response.json())
+			.then((data) => setComments(data));
+	};
+
+	useEffect(() => {
+		getComments();
+	}, []);
+
+	return (
+		<div className="comments">
+			<CommentBox />
+			{comments.map((item, _id) => {
+				return <CommentPost key={_id} name={item.user} body={item.body} />;
+			})}
+		</div>
+	);
 };
 
 export default CommentSection;
